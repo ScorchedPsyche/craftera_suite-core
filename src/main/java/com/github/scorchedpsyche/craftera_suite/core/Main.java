6f7309d7371cf0f2e0575ef481ebdbf577a7cb55
ScorchedPsyche <scorchedpsychegames@gmail.com>
2020-08-29 20:49:52 -0300

    Added StringUtils

--- src/main/java/com/github/scorchedpsyche/craftera_suite/core/Main.java
+++ src/main/java/com/github/scorchedpsyche/craftera_suite/core/Main.java
@@ -4,14 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    // Plugin startup logic
     @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+    public void onEnable()
+    {
     }
 
+    // Plugin shutdown logic
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onDisable()
+    {
     }
 }
