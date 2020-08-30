536e72bb021817dc1ae434735520700f3ab1f8b0
ScorchedPsyche <scorchedpsychegames@gmail.com>
2020-08-30 09:46:07 -0300

    Added StringUtils, renamed Main.java to CraftEraSuiteCore.java and fixed plugin prefix on plugin.yml

--- src/main/java/com/github/scorchedpsyche/craftera_suite/core/CraftEraSuiteCore.java
+++ src/main/java/com/github/scorchedpsyche/craftera_suite/core/CraftEraSuiteCore.java
@@ -1,13 +1,16 @@
 package com.github.scorchedpsyche.craftera_suite.core;
 
+import com.github.scorchedpsyche.craftera_suite.core.Utils.Natives.StringUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class CraftEraSuiteCore extends JavaPlugin {
+    public StringUtils stringUtils;
 
     // Plugin startup logic
     @Override
     public void onEnable()
     {
+        stringUtils = new StringUtils();
     }
 
     // Plugin shutdown logic
---
+++ src/main/java/com/github/scorchedpsyche/craftera_suite/core/Utils/Natives/StringUtils.java
@@ -0,0 +1,9 @@
+package com.github.scorchedpsyche.craftera_suite.core.Utils.Natives;
+
+public class StringUtils
+{
+    public boolean isEmpty(String string)
+    {
+        return string == null || string.isEmpty();
+    }
+}
